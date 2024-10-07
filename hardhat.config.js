96333865ee73e6470ee9babb4979012e908881a6
Seth Gruspe <201911598@fit.edu.ph>
2024-10-07 21:12:07 +0800

    modified hardhat configuration for contract verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,44 +1,61 @@
 require("@nomicfoundation/hardhat-toolbox");
-// require('dotenv').config();
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
 
-// const FORK_FUJI = false;
-// const FORK_MAINNET = false;
-// let forkingData = undefined;
+const FORK_FUJI = true;
+const FORK_MAINNET = false;
+let forkingData = undefined;
 
-// if (FORK_MAINNET) {
-//   forkingData = {
-//     url: "https://api.avax.network/ext/bc/C/rpcc",
-//   };
-// }
-// if (FORK_FUJI) {
-//   forkingData = {
-//     url: "https://api.avax-test.network/ext/bc/C/rpc",
-//   };
-//}
+if (FORK_MAINNET) {
+  forkingData = {
+    url: "https://api.avax.network/ext/bc/C/rpcc",
+  };
+}
+if (FORK_FUJI) {
+  forkingData = {
+    url: "https://api.avax-test.network/ext/bc/C/rpc",
+  };
+}
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
-  // networks: {
-  //   hardhat: {
-  //     gasPrice: 225000000000,
-  //     chainId: !forkingData ? 43112 : undefined, //Only specify a chainId if we are not forking
-  //     forking: forkingData,
-  //   },
-  //   fuji: {
-  //     url: "https://api.avax-test.network/ext/bc/C/rpc",
-  //     gasPrice: 225000000000,
-  //     chainId: 43113,
-  //     accounts: [process.env.WALLET_PRIVATE_KEY], // we use a .env file to hide our wallets private key
-  //   },
-  //   mainnet: {
-  //     url: "https://api.avax.network/ext/bc/C/rpc",
-  //     gasPrice: 225000000000,
-  //     chainId: 43114,
-  //     accounts: [process.env.WALLET_PRIVATE_KEY],
-  //   },
-  // },
-  // etherscan: {
-  //   apiKey: process.env.SNOWTRACE_API_KEY, // we use an .env file to hide our Snowtrace API KEY
-  // },
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {
+      gasPrice: 225000000000,
+      chainId: !forkingData ? 43112 : undefined, //Only specify a chainId if we are not forking
+      forking: forkingData,
+    },
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [process.env.PRIVATE_KEY], // we use a .env file to hide our wallets private key
+    },
+    snowtrace: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    mainnet: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      snowtrace: "snowtrace", // apiKey is not required, just set a placeholder
+    },
+    customChains: [
+      {
+        network: "snowtrace",
+        chainId: 43113,
+        urls: {
+          apiURL: "https://api.routescan.io/v2/network/testnet/evm/43113/etherscan",
+          browserURL: "https://avalanche.testnet.localhost:8080"
+        }
+      }
+    ]
+  },
 };
